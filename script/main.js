522920ab8ff77236a7c7dc60ca6824ef385e5fd9
aguscontino <agusconti1994@gmail.com>
2023-06-03 12:29:52 -0300

    incluyendo DOM, local storage, eventos, JSON; interfaz grafica

--- script/main.js
+++ script/main.js
@@ -1,42 +1,85 @@
 // BASE DE DATOS: fichas de personajes
 
 class Personaje {
-    constructor(nombre, imagen) {
+    constructor(nombre, imagen, seleccionado) {
         this.nombre = nombre
         this.imagen = imagen
+        this.seleccionado = seleccionado
     }
 }
 
 const personajes = [
-    new Personaje("Daenerys Targaryen", "imagen"),
-    new Personaje("Jon Snow", "imagen"),
-    new Personaje("Drogon", "imagen"),
-    new Personaje("Ned Stark", "imagen"),
-    new Personaje("El señor de la noche", "imagen"),
-    new Personaje("Robert Baratheon", "imagen"),
-    new Personaje("Tyrion Lannister", "imagen"),
-    new Personaje("Hodor", "imagen")
+    new Personaje("Daenerys Targaryen", "../media/personajes/daenerys-targaryen.jpg", false),
+    new Personaje("Jon Snow", "../media/personajes/jon-snow.jpg", false),
+    new Personaje("Drogon", "../media/personajes/drogon.jpg", false),
+    new Personaje("Ned Stark", "../media/personajes/ned-stark.webp", false),
+    new Personaje("El señor de la noche", "../media/personajes/night-king.webp", false),
+    new Personaje("Robert Baratheon", "../media/personajes/robert-baratheon.webp", false),
+    new Personaje("Tyrion Lannister", "../media/personajes/tyrion-lannister.jpg", false),
+    new Personaje("Hodor", "../media/personajes/hodor.jpg", false)
 ]
 
-// FUNCIONES: barajar, eliminacion, proceso principal
+// REGISTRO DE JUGADOR
 
-// barajar fichas
-function barajar(array) {
-    array.sort(() => Math.random() - 0.5)
-}
+const modal = document.getElementById("modal")
+modal.showModal()
 
-// evitando la trampa
-function antiTrampa(eleccionA, eleccionB) {
+const nombre = document.getElementById("form__nombre")
+const edad = document.getElementById("form__edad")
+const submit = document.getElementById("form__submit")
+const jugador = document.getElementById("jugador")
+const jugadorEdad = document.getElementById("jugadorEdad")
 
-    while (eleccionA === eleccionB) {
-        eleccionB = parseInt(prompt("¡NO HAGAS TRAMPA! Elegi una ficha diferente."))
+submit.addEventListener("click",()=>{
+
+    const nuevoJugador={
+        nombre: nombre.value,
+        edad: edad.value
     }
 
-    alert(`Encontraste a ${fichasBarajadas[eleccionB].nombre}.`)
+    localStorage.setItem("NuevoJugador",JSON.stringify(nuevoJugador))
+
+    const jugadorActual = JSON.parse(localStorage.getItem("NuevoJugador"))
+    jugador.innerText = jugadorActual.nombre
+    jugadorEdad.innerText = jugadorActual.edad
+
+})
+
+// SETEO DEL JUEGO
+
+const mazo = personajes.concat(personajes)
+
+const barajar = ((array) => {
+    array.sort(() => Math.random() - 0.5)
+})(mazo)
+
+const fichas = document.getElementsByClassName("tablero__ficha") //divs contenedores
+
+const dorso = document.getElementsByClassName("tablero__ficha--dorso") //img del trono
+
+for (let i = 0; i < fichas.length; i++) {
+
+    let retrato = document.createElement("img") //img de personaje
+    fichas[i].append(retrato)
+    retrato.style.display = "none"
+    retrato.src = mazo[i].imagen
+    retrato.alt = `Retrato de ${mazo[i].nombre}`
+
+    fichas[i].addEventListener("click", () => {
+
+        retrato.style.display = "block"
+        dorso[i].style.display = "none"
+
+    })
+
 }
 
-// eliminando coincidencias
-function comparacion(eleccionA, eleccionB) {
+
+
+// codigo momentaneamente en desuso
+/*
+
+function comparacion(eleccionA, eleccionB) { //eliminando coincidencias
 
     if (fichasBarajadas[eleccionA].nombre === fichasBarajadas[eleccionB].nombre) {
 
@@ -65,24 +108,4 @@ function comparacion(eleccionA, eleccionB) {
 
 }
 
-//ejecucion del juego
-function procesoPrincipal() {
-    alert("Simulacion de memotest")
-
-    const fichasBarajadas = personajes.concat(personajes)
-    barajar(fichasBarajadas)
-
-    while (fichasBarajadas.length != 0) {
-
-        let eleccionA = parseInt(prompt(`Elija una ficha indicando numero del 0 al ${fichasBarajadas.length - 1}. ¡Recuerda el numero!`))
-        alert(`Encontraste a ${fichasBarajadas[eleccionA].nombre}. ¡No lo olvides!`)
-
-        let eleccionB = parseInt(prompt(`Elija otra ficha indicando numero del 0 al ${fichasBarajadas.length - 1}.`))
-
-        antiTrampa(eleccionA, eleccionB)
-
-        comparacion(eleccionA, eleccionB)
-
-    }
-    alert("¡¡FELICITACIONES!! Encontraste todas las parejas.")
-}
+*/
